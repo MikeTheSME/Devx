e332940816c70563a400ce0e1dc0a43c7c014c19
Chaimaa <denize.ele@gmail.com>
2024-09-26 21:54:44 -0400

    chaimaa modification added

--- src/git/Class1.java
+++ src/git/Class1.java
@@ -1,7 +1,10 @@
 package git;
 
 public class Class1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         System.out.println("Hello");
+        System.out.println("chaimaa modification");
     }
 }
