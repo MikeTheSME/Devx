021327a5bca890c85f863abde247175a8bed9b60
MikeG773 <142672183+MikeG773@users.noreply.github.com>
2024-10-03 21:49:47 -0500

    Added plan comments

--- src/git/Class1.java
+++ src/git/Class1.java
@@ -4,12 +4,46 @@ public class Class1 {
     public static void main(String[] args)
     {
 
-        System.out.println("Hello");
+        System.out.println("bye");
         System.out.println("chaimaa modification");
         System.out.println("shevket modification");
         System.out.println("Hello");
         System.out.println("Have Fun!");
         System.out.println("Mike");
+
+        /*
+        Developer:
+        1. Make sure that your local master branch is up to date with your remote master branch. Switch to local master branch and then
+        pull from origin master branch.
+        Commands: git checkout master
+        git pull origin master
+        2. Create your own local branch to work on(we do not work on master branch) name it as jira ticket number hypen your name: jirahwd-mike
+        Commands: git checkout branch branchName or git checkout -b branchName       (git checkout -b branchName creates a branch and switches to it
+        3. Now you can create your code for the jira ticket on intellij
+        4. Once you are done with code go to terminal and add . and commit
+        Commands: git add . or git add theNameOfTheClass or file
+        git commit -m 'Message'
+        5. Push your local branch that you created to the remote repo make sure that the name of the branch you are pushing to is the same branch
+        name as the new local branch that you just created
+        Commands: git push origin nameOfNewLocalBranch
+        6. Create a pull request on Github the base: will be master and the branch that you are merging to master is your new branch name
+        add reviewers
+
+        Reviewers: Look over the pull request, if everything looks fine approve and merge to master branch
+
+
+        Tester:
+         1. Make sure that your local master branch is up to date with your remote master branch. Switch to local master branch and then
+        pull from origin master branch.
+        Commands: git checkout master
+        git pull origin master
+
+        2. Navigate to intellij and run code, test the behavior of the code and document results and report on confluence and Jira
+
+
+
+         */
+
 
 
     }
