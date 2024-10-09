58441fe03de5985bf92fe9e0e842452226643474
Chaimaa <denize.ele@gmail.com>
2024-10-08 20:07:07 -0400

    chaimaa modif 10/08/24

--- src/git/Class1.java
+++ src/git/Class1.java
@@ -7,6 +7,8 @@ public class Class1 {
         System.out.println("Hello");
         System.out.println("chaimaa modification");
         System.out.println("shevket modification");
+        System.out.println(" chaimaa tested shevket code");
+        System.out.println("chaimaa modif 10/08/24 at 8:05pm");
 
     }
 }
